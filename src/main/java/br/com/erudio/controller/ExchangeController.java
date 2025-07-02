a02bd342418149cbe87294cdc12d1f99569e3371
Ayrton Yoshii <itiro_yoshii@hotmail.com>
2025-07-02 00:49:09 -0300

    Aula 80 - Aula 85

--- src/main/java/br/com/erudio/controller/ExchangeController.java
+++ src/main/java/br/com/erudio/controller/ExchangeController.java
@@ -1,6 +1,8 @@
 package br.com.erudio.controller;
 
+import br.com.erudio.environment.InstanceInformationService;
 import br.com.erudio.model.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,14 @@ import java.math.BigDecimal;
 @RequestMapping("/exchange-service")
 public class ExchangeController {
 
+    @Autowired
+    InstanceInformationService informationService;
+
     // http://localhost:8000/exchange-service/5/USD/BRL
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount,
                                 @PathVariable("from") String from,
                                 @PathVariable("to") String to) {
-        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + informationService.retrieveServerPort());
     }
 }
